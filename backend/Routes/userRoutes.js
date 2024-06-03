ce6c769d3c89bfb1326d4d615f1ea175119570b2
Suryansh987 <suryanshsharma987@gmail.com>
2024-06-03 18:05:25 +0530

    fixes Cookies

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -60,7 +60,7 @@ router.post('/signin', validateRegisterData, hashPass, async (req, res) => {
         const token = createJWT(newUser)
         res
     .status(200)
-    .cookie('token', `Bearer ${token}`)
+    .cookie('token', `Bearer ${token}` ,  { expires, secure: true, httpOnly: true })
     .json({user:{
         name: newUser.name,
         email: newUser.email,
@@ -122,7 +122,7 @@ router.post('/login', validateLoginData, async(req,res)=>{
 
     res
     .status(200)
-    .cookie('token', `Bearer ${token}`,  { expires })
+    .cookie('token', `Bearer ${token}`,  { expires, secure: true, httpOnly: true })
     .json({user:{
         name: userData.name,
         email: userData.email,
